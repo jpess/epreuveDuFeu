388c2c404001e8d24d069bdac17bc204158236f6
jpess <julien.pession@gmail.com>
2022-04-11 09:44:25 +0800

    structure added

---
+++ feu00.js
@@ -0,0 +1,43 @@
+/********************
+* Echauffement
+*********************/
+
+/*
+Créez un programme qui affiche un rectangle dans le terminal.
+
+Exemples d’utilisation:
+$ > python exo.py 5 3
+o-- - o
+    |   |
+    o-- - o
+
+$ > python exo.py 5 1
+o-- - o
+
+$ > python exo.py 1 1
+o
+
+Gérer les problèmes potentiels d’arguments.
+*/
+
+/******************
+*    FUNCTIONS
+******************/
+
+/******************
+*  ERROR HANDLING
+******************/
+
+/******************
+*     PARSING
+******************/
+let result = 'error';
+
+/******************
+*      MAIN
+******************/
+
+/******************
+*     DISPLAY
+******************/
+console.log(result);
